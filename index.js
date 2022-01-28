9781d545bf5e08f14bf86f8c1ee46121d9d969df
Marek Tammevali <marek.tammevali@khk.ee>
2022-01-29 01:42:35 +0200

    post request use

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}));
+
 //add public directiory
 app.use(express.static('public'));
 //add views directory path
@@ -12,18 +15,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.get('/questions', (req, res) => {
+app.get('/login', (req, res) => {
 
-    let questions = [
-        {title: "what is node.js?", user : "Kadi", votes: "10"},
-        {title: "what is express.js?", user : "Mihkel", votes: "7"}
-    ]
+    res.render('login');
+})
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
 
-    res.render('index', {questions:questions});
+    if (username ==='user' && password ==='qwerty'){
+        res.redirect( '/dashboard');
+    }
 });
 
+app.get('/dashboard', (req, res) => {
+
+    res.render('dashboard');
+})
 
 
 app.listen(3000, () => {
-    console.log(`Server started on http://localhost:3000/questions`);
+    console.log(`Server started on http://localhost:3000/login`);
 })
