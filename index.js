aa8bfa319ff0c320135b8666eb2767624b8c0123
Marek Tammevali <marek.tammevali@khk.ee>
2022-01-29 00:59:53 +0200

    template loop use

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const app = express();
 //add public directiory
 app.use(express.static('public'));
 //add views directory path
-app.set('view', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'views'));
 
 //add views template engine
 app.set('view engine', 'ejs');
 
 
-app.get('/user/:username', (req, res) => {
+app.get('/questions', (req, res) => {
 
-    let user = req.params.username;
-    res.render('index.ejs', {username: user});
+    let questions = [
+        {title: "what is node.js?", user : "Kadi", votes: "10"},
+        {title: "what is express.js?", user : "Mihkel", votes: "7"}
+    ]
+
+    res.render('index', {questions:questions});
 });
 
 
 
-
-
-
-
 app.listen(3000, () => {
-    console.log(`Server started on http://localhost:3000`);
+    console.log(`Server started on http://localhost:3000/questions`);
 })
